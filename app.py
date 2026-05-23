2f4ccf08a48bf6ad2feda8b83269fc78fd5fa0f4
ShubhamSingh1384 <shubhamsinghjhitti143@gmail.com>
2026-05-23 18:40:18 +0530

    Added require file

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ model = joblib.load("KNN_heart.pkl")
 scaler = joblib.load("scale.pkl")
 expected_columns = joblib.load("columns.pkl")
 
-st.title("Heart Stroke Prediction by akarsh")
+st.title("Heart Stroke Prediction by SSR")
 st.markdown("Provide the following details to check your heart stroke risk:")
 
 # Collect user input
